b35f0e19b8282652d6b13b9cc8c7f96eb90999b3
Santiago Affonso <santiaffonso@gmail.com>
2025-05-13 13:58:44 -0300

    primeiro commit

---
+++ apps.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo 'Atualizando Sistema...'
+sudo apt update && sudo apt upgrade -y
+echo 'Fazendo instalação do Vim, Code, 1password'
+sudo apt install curl
+sudo apt install vim -y
+sudo snap install code --classic
+sudo snap install 1password
+sudo snap install discord --classic
+sudo apt install git -y
+echo 'Fazendo instalação do node'
+# Descarregar e instalar a nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+
+# Em vez de reiniciar a concha ou shell
+\. "$HOME/.nvm/nvm.sh"
+
+# Descarregar e instalar a Node.js:
+nvm install 22
+
+# Consultar a versão da Node.js:
+echo 'versão do node:' 
+node -v # Deveria imprimir "v22.15.0".
+echo 'versão do nvm'
+nvm current # Deveria imprimir "v22.15.0".
+echo 'versão do npm'
+# Consultar a versão da npm:
+npm -v # Deveria imprimir "10.9.2".
+
+echo 'instalação concluída'
